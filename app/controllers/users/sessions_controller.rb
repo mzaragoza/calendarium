d60acde1c99b0adff565568392357ac04ff3b994
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-10-17 15:16:38 -0400

    view for registration

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Users::SessionsController < Devise::SessionsController
+  layout 'users/login'
+  private
+  def after_sign_in_path_for(resource)
+    users_dashboard_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+end
+
